d090a169ded024f0f1ed0c525db065db5b62055f
Salima Fong <99065601@mydavinci.nl>
2020-09-27 02:21:39 +0200

    Start Rowan

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ alert("Welcome to the Sinnoh region! An adventure awaits");
 
 		check = prompt("Is" + " " + name + " " + "acceptable?");
 					if (check == "yes") {
+						document.write("Trainer ID:" + " " + name);
 						check = prompt("Are you ready for your adventure," + " " + name + "?");
 									if (check == "yes") {
 										alert("Good luck on your adventure!");
@@ -20,9 +21,10 @@ alert("Welcome to the Sinnoh region! An adventure awaits");
 
 
 		  		  } else if (check == "no") {
-					name = prompt("What is your name?");
-					check = prompt("Is" + " " + name + " " + "acceptable?");
-					check = prompt("Are you ready for your adventure," + " " + name + "?");
+						name = prompt("What is your name?");
+						check = prompt("Is" + " " + name + " " + "acceptable?");
+						document.write("Trainer ID:" + " " + name);
+						check = prompt("Are you ready for your adventure," + " " + name + "?");
 								if (check != "yes") {
 									check = prompt("Do you wish to quit?");
 												if (check == "yes") {
@@ -32,4 +34,11 @@ alert("Welcome to the Sinnoh region! An adventure awaits");
 									  }
 								}
 				}
+			
+			alert("?:" + " " + "Hello," + " " + name + "." + " " + "My name is Professor Rowan. I will be assisting you on your adventure.");
+			alert("Prof. Rowan:" + " " + "You get to choose one of my three Pokemon to help you on your journey.");
+			
+			var img = document.createElement("img");
+			img.src = "Images/chimchar.png"
+			var src = document.getElementById("x");
 			
